11f216c807ff270f38cb21024c32388e90b88d82
Songyuan Zhang <syzhang092218@gmail.com>
2025-02-24 15:38:57 -0500

    update typing

--- dgppo/utils/typing.py
+++ dgppo/utils/typing.py
@@ -13,8 +13,8 @@ Shape = Tuple[int, ...]
 
 BFloat = Float[Array, "b"]
 BInt = Int[Array, "b"]
-FloatScalar = float | Float[Array, ""]
-IntScalar = int | Int[Array, ""]
+FloatScalar = Float[Array, ""]
+IntScalar = Int[Array, ""]
 TFloat = Float[Array, "T"]
 
 # environment types
@@ -25,13 +25,13 @@ Done = BoolScalar
 Info = Dict[str, Shaped[Array, '']]
 EdgeIndex = Float[Array, '2 n_edge']
 AgentState = Float[Array, 'num_agents agent_state_dim']
-State = Float[Array, 'num_states state_dim'] | type
+State = Float[Array, 'num_states state_dim']
 Node = Float[Array, 'num_nodes node_dim']
 EdgeAttr = Float[Array, 'num_edges edge_dim']
 Pos2d = Float[Array, '2'] | Float[ndarray, '2']
 Pos3d = Float[Array, '3'] | Float[ndarray, '3']
 Pos = Pos2d | Pos3d
-Radius = Float[Array, ''] | float
+Radius = Float[Array, '']
 
 
 # neural network types
